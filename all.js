4b7a670cf20865a8188f74b1486bd1a7209d3229
Reiracode <reira.igg@gmail.com>
2021-10-12 11:25:42 +0800

    SnakeAnim

--- all.js
+++ all.js
@@ -1,13 +1,97 @@
 let yourPositon, infoData, filterData;
 const loading = document.getElementById("loading");
 const maskSize = document.querySelector("#mask_sel");
+// 洧杰申請
+//   let AppID = '4ad9f73726a0409a9376afd2b59e59a7';
+//  let AppKey = 'iR-j7mJI1CY924a-xfd6vhXZciM';
+
+function getAuthorizationHeader() {
+  //  填入自己 ID、KEY 開始
+  let AppID = "4ad9f73726a0409a9376afd2b59e59a7";
+  let AppKey = "iR-j7mJI1CY924a-xfd6vhXZciM";
+  //  填入自己 ID、KEY 結束
+  let GMTString = new Date().toGMTString();
+  let ShaObj = new jsSHA("SHA-1", "TEXT");
+  ShaObj.setHMACKey(AppKey, "TEXT");
+  ShaObj.update("x-date: " + GMTString);
+  let HMAC = ShaObj.getHMAC("B64");
+  let Authorization =
+    'hmac username="' +
+    AppID +
+    '", algorithm="hmac-sha1", headers="x-date", signature="' +
+    HMAC +
+    '"';
+  return { Authorization: Authorization, "X-Date": GMTString };
+}
+
 const api =
   "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?$top=300&$format=JSON";
-// 25.0224296 121.4994827
-// const api =
-//   "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?$top=1000&$spatialFilter=nearby(25.0224296%2C121.4994827%2C30000000)&$format=JSON";
-const jpg =
-  "https://images.unsplash.com/photo-1550760146-f2f4cf8961f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80";
+
+// get bike position nearby
+let a1 =
+  "https://ptx.transportdata.tw/MOTC/v2/Bike/Station/NearBy?$top=30&$spatialFilter=nearby(25.0375421148651%2C%20121.50675594797%2C%201000)&$format=JSON";
+let a2 =
+  "https://ptx.transportdata.tw/MOTC/v2/Bike/Availability/NearBy?$top=30&$spatialFilter=nearby(25.0375421148651%2C%20121.50675594797%2C%201000)&$format=JSON";
+
+function mergeArrayObjects(a1, a2) {
+  return a1.map((item, i) => {
+    if (item.StationUID === a2[i].StationUID) {
+      return Object.assign({}, item, a2[i]);
+    }
+  });
+}
+//
+let bike_path =
+  "https://ptx.transportdata.tw/MOTC/v2/Cycling/Shape/NewTaipei?$top=5500&$format=JSON";
+// let bike_path = "https://ptx.transportdata.tw/MOTC/v2/Cycling/Shape/Taipei?$top=500&$format=JSON"
+
+const getBike = async () => {
+  const response = await fetch(a1, {
+    headers: getAuthorizationHeader(),
+  });
+  const json = await response.json();
+  return json;
+};
+
+const getBike1 = async () => {
+  const response = await fetch(a2, {
+    headers: getAuthorizationHeader(),
+  });
+  const json = await response.json();
+  return json;
+};
+
+const getPath = async () => {
+  const response = await fetch(bike_path, {
+    headers: getAuthorizationHeader(),
+  });
+  const json = await response.json();
+  return json;
+};
+
+let ballsssss, tesing, a3;
+async function getBikes() {
+  const [a1, a2, bikepath] = await Promise.all([
+    getBike(),
+    getBike1(),
+    getPath(),
+  ]);
+
+  // bike position avaiable
+  a3 = mergeArrayObjects(a1, a2);
+  console.log(a3);
+  console.log(bikepath);
+  tesing = bikepath;
+  // bike path
+  ballsssss = bikepath.filter(({ RouteName, Geometry }) => {
+    return RouteName.indexOf("自行車道") > -1;
+  });
+
+  console.log(ballsssss);
+}
+
+getBikes();
+
 let mymarker,
   map,
   mymap,
@@ -51,6 +135,13 @@ function createMaskIcon(path) {
     iconSize: [60, 60],
     iconAnchor: [16, 36],
     popupAnchor: [1, -34],
+  });
+}
+
+function createDivIcon(name) {
+  return new L.divIcon({
+    html: '<div class="divicon">' + name + "</div>",
+    iconSize: [0, 0],
   });
 }
 
@@ -117,7 +208,7 @@ function drawMap() {
   markers = L.markerClusterGroup({
     maxClusterRadius: 120,
     iconCreateFunction: function (cluster) {
-      let childCount = cluster.getChildCount();
+      let childCount = cluster.getChildCount() / 2;
       let group = " marker-cluster-";
       if (childCount < 5) {
         group += "small";
@@ -134,12 +225,15 @@ function drawMap() {
     },
   });
 
-  const attribution =
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-  const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-  const tiles = L.tileLayer(tileUrl, { attribution });
-  tiles.addTo(mymap);
-  getMymarker();
+
+
+
+
+    const attribution =
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
 }
 
 //重新整理
@@ -161,13 +255,13 @@ function getMymarker() {
   function twnIcons() {
     return new L.divIcon({
       html: '<i class="twicon-san-domingo"></i>',
-      iconSize: [60, 60],
+      iconSize: [30, 30],
       className: "myDivIcon",
     });
   }
   const fontAwesomeIcon = L.divIcon({
     html: '<i class="twicon-san-domingo"></i>',
-    iconSize: [60, 60],
+    iconSize: [30, 30],
     className: "myDivIcon",
   });
 
@@ -183,8 +277,8 @@ function getDistance(origin, destination) {
   lng1 = origin[1];
   lat2 = destination[0];
   lng2 = destination[1];
-  // console.log(lat1, lng1)
-  //   console.log(lat2, lng2);
+  console.log(lat1, lng1)
+    console.log(lat2, lng2);
   return (
     2 *
     6378.137 *
@@ -228,22 +322,21 @@ function markClick(event) {
   // console.log(markIndex);
   //scroll對應到overlay id的位子點地圖上icon位置的 ，找到click id的index 15
   // if (!!document.querySelector(".overlay.active>.datalist")) {
-    let overlaylist = document.querySelector(".overlay.active>.datalist");
-    let divid = overlaylist.id;
-    let all = document.querySelectorAll(`#${divid}>.spot_detail`);
-    marginBtom = all[0].marginBottom;
-    let margbtm = marginBtom.substr(0, marginBtom.length - 2);
-    //5px  scrollto 只算前段的div的高度  index=5  len=6 calcu div*index==>index 3  ==>  0+1+2
-    let sum = 0;
-    for (let i = 0; i < markIndex; i++) {
-      sum += parseInt(all[i].scrollWidth) + parseInt(margbtm);
-    }
-    document.getElementById(`${divid}`).scrollTo(0, sum);
+  let overlaylist = document.querySelector(".overlay.active>.datalist");
+  let divid = overlaylist.id;
+  let all = document.querySelectorAll(`#${divid}>.spot_detail`);
+  marginBtom = all[0].marginBottom;
+  let margbtm = marginBtom.substr(0, marginBtom.length - 2);
+  //5px  scrollto 只算前段的div的高度  index=5  len=6 calcu div*index==>index 3  ==>  0+1+2
+  let sum = 0;
+  for (let i = 0; i < markIndex; i++) {
+    sum += parseInt(all[i].scrollWidth) + parseInt(margbtm);
+  }
+  document.getElementById(`${divid}`).scrollTo(0, sum);
   // }
 }
 
 //select all / adult/ child
- 
 
 //************************************************
 function onMapLoad() {
@@ -253,7 +346,7 @@ function onMapLoad() {
 function twnIcons(path) {
   return new L.divIcon({
     html: `<i class="twicon-${path}"></i>`,
-    iconSize: [60, 60],
+    iconSize: [30, 30],
     className: "myDivIcon",
   });
 }
@@ -266,6 +359,40 @@ function renderMask() {
   let s_list = document.querySelector(".datalist");
   let focus = filterData[0].Position;
   mymap.setView([focus.PositionLat, focus.PositionLon], 15);
+  // StationPosition: GeoHash: "wsqqkyxyv";
+  // PositionLat: 25.041778;
+  // PositionLon: 121.508693;
+  let bikedata = a3.sort(({ StationPosition: a }, { StationPosition: b }) => {
+    console.log(a)
+    return (
+      getDistance(
+        [yourPositon[0], yourPositon[1]],
+        [a.PositionLat, a.PositionLon]
+      ) -
+      getDistance(
+        [yourPositon[0], yourPositon[1]],
+        [b.PositionLat, b.PositionLon]
+      )
+    );
+
+
+
+  });
+  console.log(bikedata);
+  console.log(yourPositon);
+let sss = bikedata.forEach((item) => {
+    // console.log(item.StationPosition);
+    let distance = getDistance(
+      [yourPositon[0], yourPositon[1]],
+      [item.StationPosition.PositionLat, item.StationPosition.PositionLon]
+    );
+   
+  console.log(distance)
+  
+  
+});
+
+
 
   let el = "";
   let alldata = filterData.sort(({ Position: a }, { Position: b }) => {
@@ -281,8 +408,7 @@ function renderMask() {
     );
   });
 
-
-//  san-domingo
+  //  san-domingo
   alldata.forEach((item) => {
     let cion;
     if (!item.Name.includes("總統府")) {
@@ -299,13 +425,28 @@ function renderMask() {
     mask = !!item.Picture.PictureUrl1
       ? !!item.Name.includes("總統府")
         ? twnIcons("president-office")
-        : createMaskIcon(jpg)
+        : createMaskIcon(item.Picture.PictureUrl1)
       : createIcons("factory");
 
     let marker = L.marker(
       new L.LatLng(item.Position.PositionLat, item.Position.PositionLon),
-      {icon: mask,}
+      { icon: mask }
     );
+
+    // let divIcon = L.divIcon({
+    //   html: '<div class="divicon">' + item.Name + "</div>",
+    //   iconSize: [0, 0],
+    // });
+
+    // L.marker([item.Position.PositionLat, item.Position.PositionLon], {
+    //   icon: divIcon,
+    // }).addTo(mymap);
+
+    let divmark = L.marker(
+      new L.LatLng(item.Position.PositionLat, item.Position.PositionLon),
+      { icon: createDivIcon(item.Name) }
+    );
+
     //********************************************* */
     let distance = getDistance(
       [yourPositon[0], yourPositon[1]],
@@ -315,7 +456,7 @@ function renderMask() {
           ${
             !!item.Picture.PictureUrl1
               ? `<div class="detail_img">
-                    <img src=${jpg} alt="" />
+                    <img src=${item.Picture.PictureUrl1} alt="" />
                 </div>`
               : " "
           }
@@ -334,12 +475,20 @@ function renderMask() {
     marker.on("mouseover", function (ev) {
       marker.openPopup();
     });
+
+    // L.marker([item.Position.PositionLat, item.Position.PositionLon], {
+    //   icon: divIcon,
+    // }).addTo(mymap);
+
     markersRef.push(marker);
     markers.addLayer(marker);
+
+    markers.addLayer(divmark);
   });
 
   s_list.scrollTo(0, 0);
   mymap.addLayer(markers);
+
   //marker 點時，算高度到scroll如果有資料，scroll to index
   markers.on("click", (event) => {
     let id = event.layer._leaflet_id;
@@ -349,7 +498,7 @@ function renderMask() {
     el = `${
       !!alldata[markIndex].Picture.PictureUrl1
         ? `<div class="sidedetail_img">
-              <img src="${jpg}" alt="" />
+              <img src="${alldata[markIndex].Picture.PictureUrl1}" alt="" />
           </div>`
         : " "
     }
@@ -358,6 +507,11 @@ function renderMask() {
         <a href="${alldata[markIndex].WebsiteUrl}" target="_blank">
         ${alldata[markIndex].Name}</a>
       </h2>
+      <a target="_blank" href="https://www.google.com/search?tbm=isch&q=${
+        alldata[markIndex].Name
+      }">
+      XXXX
+      </a>
       <p><i class="far fa-clock"></i> ${alldata[markIndex].OpenTime}</p>
       <div class="hook">
         <p> ${alldata[markIndex].DescriptionDetail}</p>
@@ -366,33 +520,73 @@ function renderMask() {
     </div>`;
     info.innerHTML = el;
 
-
     let all = document.querySelectorAll(`#${s_list.id}>.spot_detail`);
-    console.log(all)
-      marginBtom = getComputedStyle(all[0]).marginBottom;
-      let margbtm = marginBtom.substr(0, marginBtom.length - 2);
-      sum = 0;
-      for (let i = 0; i < markIndex; i++) {
-        console.log(all[i].scrollWidth);
-        sum += parseInt(all[i].scrollWidth) + parseInt(margbtm);
-      }
-      document.getElementById(`${s_list.id}`).scrollTo({
-        left: sum,
-        behavior: "smooth",
-      });
+    console.log(all);
+    marginBtom = getComputedStyle(all[0]).marginBottom;
+    let margbtm = marginBtom.substr(0, marginBtom.length - 2);
+    sum = 0;
+    for (let i = 0; i < markIndex; i++) {
+      console.log(all[i].scrollWidth);
+      sum += parseInt(all[i].scrollWidth) + parseInt(margbtm);
+    }
+    document.getElementById(`${s_list.id}`).scrollTo({
+      left: sum,
+      behavior: "smooth",
+    });
     // }
   });
+
   mymap.doubleClickZoom.disable();
   checkInfox = document.querySelector(".close_overlay");
   checkInfox.addEventListener("click", getStore);
 
   sideInfo = [...s_list.children];
   sideInfo.forEach((dom) => dom.addEventListener("click", getPoint));
-
   // document.querySelector(".view_all").addEventListener("click", readAll);
+  //add more latitude and longitude
+  console.log(tesing);
+  // bike path
 
+  // var a = ballsssss.map((items) => {
+  var a = tesing
+    .filter(({ RouteName }) => {
+      return RouteName.indexOf("自行車道") > -1;
+    })
+    .map((items) => {
+      let sns = items.Geometry.replace("MULTILINESTRING ((", "")
+        .replaceAll("))", "")
+        .replaceAll(")", "")
+        .replaceAll("(", "")
+        .split(",");
+
+      let newsa = sns.map((i) => {
+        return i.split(" ").reverse();
+      });
+
+      items.geolocation = newsa;
+      return items;
+    });
+
+  console.log(a); //no reverse
+  let route = a[0].geolocation;
+  console.log(route); //no reverse
+
+  for (var i = 0, latlngs = [], len = route.length; i < len; i++) {
+    latlngs.push(new L.LatLng(route[i][0], route[i][1]));
+  }
+  console.log(latlngs[0]);
+  console.log(latlngs[len - 1]);
+
+  var path = L.polyline(latlngs);
+  //  var map = L.map("map");
+  mymap.fitBounds(L.latLngBounds(latlngs));
+  mymap.addLayer(L.marker(latlngs[0]));
+  mymap.addLayer(L.marker(latlngs[len - 1]));
+
+  mymap.addLayer(path);
+  path.snakeIn();
 }
- 
+
 function getStore(e) {
   // document.querySelector(".close_overlay").addEventListener("click", (e) => {
   console.log(e);
@@ -413,24 +607,55 @@ function getPoint() {
   }
 }
 function readAll() {
-  console.log(e.parentNode)
+  console.log(e.parentNode);
 }
-
 
 info.addEventListener("click", (e) => {
   const ta = e.target;
   e.stopPropagation();
   console.log(ta);
   if (ta.className == "view_all") {
-    document.querySelector(".detail_body .hook").classList.toggle("open");;
-    
+    document.querySelector(".detail_body .hook").classList.toggle("open");
   }
 });
 
+// }).addTo(mymap);
+let searchbike = document.getElementById("findspots");
+// searchbike.addEventListener("click mouseover keypress", (e) => {
+//   // e.currentTarget.classList.add("click");
+//   console.log(e);
+// });
 
-// L.Routing.control({
-//     waypoints: [L.latLng(49.47748, 8.42216), L.latLng(49.47648, 8.32216)],
-//     routeWhileDragging: true
-//   }).addTo(map);
-  
+searchbike.addEventListener("keyup", (e) => {
+  console.log(e.code);
+  //    e.currentTarget.classList.add("click");
+  switch (e.code) {
+    case "Enter":
+      console.log("Enter filter");
+      filterPoints();
 
+    case "click":
+
+    default:
+      break;
+  }
+});
+
+searchbike.addEventListener("mousedown", (e) => {
+  e.currentTarget.classList.add("click");
+});
+
+//filter by select option
+function filterPoints() {
+  let string = searchbike.value;
+  console.log(string);
+  const res = infoData.filter(({ Name }) => {
+    return Name.indexOf(string) !== -1;
+  });
+
+  console.log(infoData);
+  console.log(res);
+
+  filterData = res;
+  renderMask();
+}
